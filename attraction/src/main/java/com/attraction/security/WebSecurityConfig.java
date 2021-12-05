3e625167a16cd86f1ffcbc857f914e596e544105
Jan Chmielewski <jjjchmielewski@gmail.com>
2021-12-05 16:40:03 +0100

    Login mechanism working

--- attraction/src/main/java/com/attraction/security/WebSecurityConfig.java
+++ attraction/src/main/java/com/attraction/security/WebSecurityConfig.java
@@ -2,21 +2,13 @@ package com.attraction.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,47 +20,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-            .csrf().disable()
-            .authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated()
-            .and()
-            .formLogin().loginPage("/login").permitAll()
-            .and()
-            .httpBasic()
-            .and()
-            .sessionManagement().disable()
-            .logout().permitAll();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new PasswordEncoder() {
-      @Override
-      public String encode(CharSequence rawPassword) {
-        return Base64.getEncoder().encodeToString(rawPassword.toString().getBytes(StandardCharsets.UTF_8));
-      }
-
-      @Override
-      public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        return false;
-      }
-    };
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/signup").permitAll()
+            .and().csrf().disable().authorizeRequests().anyRequest().authenticated()
+            .and().formLogin()
+            .loginPage("/login").failureUrl("/login?error=true")
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .and().logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/").and().exceptionHandling();
   }
 
   @Override
   public void configure(AuthenticationManagerBuilder builder) throws Exception {
-    builder.userDetailsService(userDetailsService);
+    builder
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(new BCryptPasswordEncoder());
   }
 
-  //user credentials for testing purposes
-  @Bean
-  @Override
-  public UserDetailsService userDetailsService() {
-    UserDetails user =
-            User.withDefaultPasswordEncoder()
-                    .username("user")
-                    .password("password")
-                    .roles("USER")
-                    .build();
-    return new InMemoryUserDetailsManager(user);
-  }
 }
