6016eae8927f5f98c642cbb55f7c5152862594c0
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2021-12-05 10:28:59 +0100

    Add basic database communication

--- attraction/src/test/java/com/attraction/AppTest.java
+++ attraction/src/test/java/com/attraction/AppTest.java
@@ -1,20 +1,37 @@
 package com.attraction;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import com.attraction.monument.Monument;
+import com.attraction.monument.MonumentRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void createObjectAndGetIt(@Autowired MonumentRepository monumentRepo) {
+        Monument object = new Monument(1, "name", 0, 10.0, 20.0, "Nice object", "there will be type", "will be",
+                "Warsaw");
+        monumentRepo.save(object);
+        System.out.println();
+        assertEquals(monumentRepo.findById(1).get().getName(), object.getName());
+    }
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
     }
 }
