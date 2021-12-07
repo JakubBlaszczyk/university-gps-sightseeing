74b107b735e23c988c785bdab540e1dc46983153
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2021-12-07 23:25:25 +0100

    Update UserPanelController
    There is need to change password with POST request.

--- attraction/src/main/java/com/attraction/user/UserPanelController.java
+++ attraction/src/main/java/com/attraction/user/UserPanelController.java
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -18,5 +19,13 @@ public class UserPanelController {
   @PreAuthorize("hasRole('USER') or hasRole('GUIDE') or hasRole('ADMIN')")
   public UserObject loadPanel(@PathVariable String username) {
     return userService.findByUsername(username);
+  }
+
+  @PostMapping("/user/{username}/change/password/{password}")
+  @PreAuthorize("hasRole('USER') or hasRole('GUIDE') or hasRole('ADMIN')")
+  public void changePassword(@PathVariable String username, @PathVariable String password) {
+    UserObject userObject = userService.findByUsername(username);
+    userService.save(new UserObject(userObject.getId(), userObject.getUsername(), password, userObject.getEmail(),
+        userObject.getAvatar(), userObject.getPoints(),userObject.getPreference(), userObject.getRoles()));
   }
 }
