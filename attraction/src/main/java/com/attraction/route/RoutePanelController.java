3993942ff0f4d205dbfc260b90fa9bd9ae5b4a16
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2021-12-28 20:12:29 +0100

    Add single route retrieval

--- attraction/src/main/java/com/attraction/route/RoutePanelController.java
+++ attraction/src/main/java/com/attraction/route/RoutePanelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +13,17 @@ public class RoutePanelController {
 
   @Autowired
   private RouteService routeService;
+
   @GetMapping("/routes")
   @PreAuthorize("hasAnyAuthority('USER', 'GUIDE', 'ADMIN')")
   public List<Route> loadPanel() {
     return routeService.getAllRoutes();
+  }
+
+  @GetMapping("/route/{id}")
+  @PreAuthorize("hasAnyAuthority('USER', 'GUIDE', 'ADMIN')")
+  public Route loadRoute(@PathVariable Integer id) {
+    return routeService.getRoute(id);
   }
 
 }
--- attraction/src/main/java/com/attraction/route/RouteService.java
+++ attraction/src/main/java/com/attraction/route/RouteService.java
@@ -1,6 +1,7 @@
 package com.attraction.route;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,10 @@ public class RouteService {
   public List<Route> getAllRoutes() {
     return routeRepository.findAll();
   }
-  
+
+  public Route getRoute(Integer id) {
+    Optional<Route> result = routeRepository.findById(id);
+    return result.isPresent() ? result.get() : null;
+  }
+
 }
