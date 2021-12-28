ea3936177f9b0c1dcd2a776137b44f6a2c51fc12
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2021-12-28 20:03:58 +0100

    Add device

---
+++ attraction/src/main/java/com/attraction/device/DeviceRepository.java
@@ -0,0 +1,7 @@
+package com.attraction.device;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DeviceRepository extends MongoRepository<Device, String> {
+  
+}
