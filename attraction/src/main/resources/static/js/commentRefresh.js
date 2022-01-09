0e13db1cb521bee7a123e421094455f253a9187c
Jan Chmielewski <jjjchmielewski@gmail.com>
2022-01-09 22:49:56 +0100

    Add 'location-in-use' information text

--- attraction/src/main/resources/static/js/commentRefresh.js
+++ attraction/src/main/resources/static/js/commentRefresh.js
@@ -7,8 +7,12 @@ $(document).ready(function() {
     var longi = document.getElementById("longitude");
     var lati = document.getElementById("latitude");
 
+    $('.location-in-use').hide();
+
     function getLocation() {
         navigator.geolocation.getCurrentPosition(sendPosition);
+        $('.location-in-use').show();
+        setTimeout(() => {$('.location-in-use').hide();}, 3000);
     }
 
     function sendPosition(position) {
